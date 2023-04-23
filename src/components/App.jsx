69d03c163ba26b0fce9c90b9d3c5d96b9240c907
Danilo Korol <777danilkorol777@gmail.com>
2023-04-23 12:59:29 +0300

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,121 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 import css from './App.module.css';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setbad] = useState(0);
+
+  const options = ['good', 'neutral', 'bad'];
+
+  const addFeedback = name => {
+    switch (name) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setbad(prevBad => prevBad + 1);
+        break;
+      default:
+        break;
+    }
   };
 
-  addFeedback = name => {
-    this.setState(prevState => {
-      return {
-        [name]: prevState[name] + 1,
-      };
-    });
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    const { good } = this.state;
+  const total = countTotalFeedback();
+
+  const countPositiveFeedbackPercentage = () => {
     return Math.round((good * 100) / total);
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const keys = Object.keys(this.state);
-
-    return (
-      <div className={css.container}>
-        <h1 className={css.title}>Expresso</h1>
-        <Section title="Please leave feedback">
-          <FeedbackOptions options={keys} onLeaveFeedback={this.addFeedback} />
-        </Section>
-        <Section title="Statistics">
-          {total ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={this.countPositiveFeedbackPercentage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>Expresso</h1>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={addFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={countPositiveFeedbackPercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   addFeedback = name => {
+//     this.setState(prevState => {
+//       return {
+//         [name]: prevState[name] + 1,
+//       };
+//     });
+//   };
+
+//   countTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   };
+
+//   countPositiveFeedbackPercentage = () => {
+//     const total = this.countTotalFeedback();
+//     const { good } = this.state;
+//     return Math.round((good * 100) / total);
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const total = this.countTotalFeedback();
+//     const keys = Object.keys(this.state);
+
+//     return (
+//       <div className={css.container}>
+//         <h1 className={css.title}>Expresso</h1>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions options={keys} onLeaveFeedback={this.addFeedback} />
+//         </Section>
+//         <Section title="Statistics">
+//           {total ? (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={total}
+//               positivePercentage={this.countPositiveFeedbackPercentage}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
